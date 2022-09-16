617d6ed1c242f51e3528a9d4acdb3d25498fef6c
shtayeb <shahriar.safi@gmail.com>
2022-09-16 22:45:13 +0430

    Done: until user model modification

--- MyDjango/settings.py
+++ MyDjango/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
 ROOT_URLCONF = 'MyDjango.urls'
@@ -132,3 +137,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- base/forms.py
+++ base/forms.py
@@ -1,16 +1,16 @@
-from django.contrib.auth.models import User
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
+
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
-        exclude = ['host','participants']
-        
-        
+        exclude = ['host', 'participants']
+
+
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username','email']
-    
+        fields = ['username', 'email']
