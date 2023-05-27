533b955b5edb6b92e3f469d6d3c14b2c6ed69c4b
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-05-27 15:38:21 +0430

    added inline relationship in admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Room, Topic, Message, User
 
-admin.site.register(Room)
+class ParticipantsInline(admin.TabularInline):
+    model = Room.participants.through
+
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [
+        ParticipantsInline,
+    ]
+
+admin.site.register(Room,RoomAdmin)
+
+
 admin.site.register(Topic)
 admin.site.register(Message)
 admin.site.register(User)
