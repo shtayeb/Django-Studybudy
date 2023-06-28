ab8433a0e55e68f1b80ccfca14d28049566a3770
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-06-28 19:04:38 +0430

    added static root

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,7 @@ MEDIA_URL = "/images/"
 
 AUTH_USER_MODEL = "accounts.User"
 
-# STATIC_ROOT =
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
