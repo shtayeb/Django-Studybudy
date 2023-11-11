7c74447c75dfa3f1946b47c0f046b9180c2d7ab8
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-11-11 16:07:26 +0430

    WIP

--- src/base/tests/test_views.py
+++ src/base/tests/test_views.py
@@ -1,12 +1,95 @@
 from unittest import skip
 
-from django.http import HttpRequest
+# from django.http import HttpRequest
 from django.test import Client, TestCase
 from django.urls import reverse
 
 from base.models import Room, Topic, User
-from base.views import home
+
+# from base.views import home
 from http import HTTPStatus
+
+# Import the modules you need
+# Import the modules you need
+# from django.urls import resolve
+from base import views
+import inspect  # Import the inspect module to check the source of the functions
+
+# Import the modules you need
+from django_extensions.management.commands.show_urls import Command as ShowUrlsCommand
+import json
+# Define a test case class
+
+
+class ViewTestCase(TestCase):
+    def get_all_base_urls(self):
+        # Create an instance of the show_urls command
+        show_urls = ShowUrlsCommand()
+        # Call the handle method with the desired arguments
+        # For example, you can pass the app name to filter the urls by app
+        output = show_urls.handle(
+            appname="base",
+            no_color=True,
+            language="en",
+            decorator="",
+            unsorted="",
+            format_style="json",
+            urlconf="ROOT_URLCONF_BASE",
+            traceback="",
+        )
+
+        output_list = json.loads(output)
+
+        return output_list
+
+        # print(output_list[0]["module"])
+        # Assert that the output contains the expected urls
+        # self.assertIn('/service-session/status django_app.views.service_session_status', output)
+
+    # Define a helper method to get all the view functions in views.py
+    def get_view_functions(self):
+        # Get all the attributes of the views module
+        attrs = dir(views)
+        # Filter out the ones that start with underscore or are not callable
+        funcs = [getattr(views, attr) for attr in attrs if not attr.startswith("_") and callable(getattr(views, attr))]
+        # Filter out the ones that are imported from other modules
+        funcs = [
+            func for func in funcs if inspect.getmodule(func) == views
+        ]  # Check if the function's module is the same as the views module
+        # Return the list of functions
+        return funcs
+
+    # Define a test method to check if all the view functions are used in urls
+    def test_view_functions_are_used_in_urls(self):
+        # Get all the view functions
+        view_funcs = self.get_view_functions()
+        view_urls = self.get_all_base_urls()
+
+        # Loop through each function
+        for func in view_funcs:
+            # Try to resolve the function name as a url pattern name
+            # TODO: We cant get the path from view_name without its url params.
+            # And the url param is defferent for all of it.
+            found = False
+            for url in view_urls:
+                if func.__name__ in url["module"]:
+                    found = True
+                    break
+
+            if not found:
+                self.fail(f"{func.__name__} is not used in urls")
+                # print(f"View: {func.__name__}")
+                # resolver = resolve(f"/{func.__name__}/")
+                # path = reverse(func.__name__)
+                # print(f"View: {func.__name__} has Path: {path}")
+                # Assert that the resolver matches the function
+                # self.assertTrue(resolve)
+                # self.assertEqual(resolve(reverse(func.__name__)).func.__name__, func.__name__)
+                # self.fail(f"{func.__name__} is not used in urls: {e}")
+
+            # try:
+            # If the function name is not a valid url pattern name, raise an assertion error
+            # except Exception as e:
 
 
 class RobotsTxtTests(TestCase):
@@ -29,6 +112,7 @@ class TestSkip(TestCase):
         pass
 
 
+@skip("demonstrating skip 2")
 class TestViewResponses(TestCase):
     def setUp(self) -> None:
         super().setUp()
@@ -53,12 +137,10 @@ class TestViewResponses(TestCase):
         """
         Example: code validation, search HTML for text
         """
-        request = HttpRequest()
-        response = home(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>StudyBuddy - Find study partners around the world!</title>", html)
-        self.assertTrue(html.find("\n<!DOCTYPE html>\n"))
-        self.assertEqual(response.status_code, 200)
+        # response = self.c.get(reverse("home"))
+        # self.assertEqual(response.status_code, 200)
+        # self.assertIn("<title>StudyBuddy - Find study partners around the world!</title>", response.content)
+        # self.assertTrue(response.content.find("\n<!DOCTYPE html>\n"))
 
     def test_url_allowed_hosts_2(self):
         response = self.c.get("/", HTTP_HOST="noaddresss.com")
