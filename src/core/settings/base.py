4e4157167f7d1c05caf86d107cedda081abd7bbc
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-02-06 16:19:45 +0430

    changes for render

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = [
     # my apps
     "accounts.apps.AccountsConfig",
     "base.apps.BaseConfig",
+    "django_tui",
 ]
 
 MIDDLEWARE = [
--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -1,5 +1,9 @@
 # ruff: noqa
 from .base import *
+import dj_database_url
+import os
+
+
 import sentry_sdk
 
 SENTRY_DSN = env("SENTRY_DSN", default="")
@@ -8,15 +12,16 @@ SENTRY_DSN = env("SENTRY_DSN", default="")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": env("DB_NAME", default="test"),
-        "USER": env("DB_USER", default="root"),
-        "PASSWORD": env("DB_PASSWORD", default=""),
-        "HOST": env("DB_HOST", default="localhost"),
-        "PORT": env("DB_PORT", default="3306"),
-        "OPTIONS": {"charset": "utf8mb4"},
-    },
+    "default": dj_database_url.parse(env("DATABASE_URL", default="local"))
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": env("DB_NAME", default="test"),
+    #     "USER": env("DB_USER", default="root"),
+    #     "PASSWORD": env("DB_PASSWORD", default=""),
+    #     "HOST": env("DB_HOST", default="localhost"),
+    #     "PORT": env("DB_PORT", default="3306"),
+    #     "OPTIONS": {"charset": "utf8mb4"},
+    # },
 }
 
 
